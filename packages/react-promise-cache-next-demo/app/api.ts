901ba2cbdd5632b2ec8584e66f0e464d32187d95
Mohamed EL AYADI <moh.el.ayadi@gmail.com>
2023-04-16 23:25:58 +0000

    readme and 0.0.7

--- packages/react-promise-cache-next-demo/app/api.ts
+++ packages/react-promise-cache-next-demo/app/api.ts
@@ -1,19 +1,5 @@
 import axios from "axios";
-import {patchQuery, patchMutation} from "react-promise-cache";
 
 export const API = axios.create({
   baseURL: "https://jsonplaceholder.typicode.com",
 });
-
-// for some reason, direct assignment throws a ts error, to be fixed
-let patchedGet = patchQuery(API.get, (url) => url)
-let patchedPut = patchMutation(API.put, url => url)
-let patchedPost = patchMutation(API.post, url => url)
-let patchedPatch = patchMutation(API.patch, url => url)
-let patchedDelete = patchMutation(API.delete, url => url)
-
-API.get = patchedGet
-API.put = patchedPut
-API.post = patchedPost
-API.patch = patchedPatch
-API.delete = patchedDelete
