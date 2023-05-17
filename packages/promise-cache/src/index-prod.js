2ddda468f7e6dbd951f6c865d40004c4bcce33e6
Abderrahim SOUBAI-ELIDRISI <gx.coder@gmail.com>
2023-05-17 18:26:26 +0100

    chore: use safe nullish operator for env

--- packages/promise-cache/src/index-prod.js
+++ packages/promise-cache/src/index-prod.js
@@ -1,7 +1,5 @@
 "use strict";
 
-if (process.env.NODE_ENV === "production") {
-	module.exports = require("./react-promise-cache.production.js");
-} else {
-	module.exports = require("./react-promise-cache.development.js");
-}
+const env = process.env.NODE_ENV ?? "development";
+module.exports = require(`./react-promise-cache.${env}.js`);
+
